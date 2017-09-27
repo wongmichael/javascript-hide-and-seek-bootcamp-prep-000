ac666b8415a6087c8b5d37806754a2be6f6bd0f4
Michael Wong <mik9080@gmail.com>
2017-09-27 22:06:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(sel){
+  return document.querySelector(sel)
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('.ranked-list li')
+  for (var i=0;i<lis.length;i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+  }
+}
+function deepestChild(){
+  var current = document.getElementById('grand-node').querySelectorAll('div')
+  return current[current.length-1]
+  var next = []
+  while (current) {
+    if (!Array.isArray(current)) {
+      return current
+    }
+    for (var i=0;i<current.length;i++){
+      next.push(current[i])
+    }
+    current = next.shift()
+  }
+  return null
+}
